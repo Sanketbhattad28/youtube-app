f7643154b7008fe0d993428ad77689feb5a7110a
Sanketbhattad28 <snktbhattad@gmail.com>
2022-09-11 16:05:14 +0530

    Changed API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
+const KEY = "AIzaSyCpzypSyDeXADxn3TIZCEBKx3E4ePo81VQ";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
